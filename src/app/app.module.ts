55b33dc510b65b39cf432363747ebaa399e9e000
EreckGordon <ereckgordon@gmail.com>
2017-08-21 21:27:06 -0700

    angular cli + universal + pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'ng-iso'})
   ],
   providers: [],
   bootstrap: [AppComponent]
