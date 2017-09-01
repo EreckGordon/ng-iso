07943876e575987d724249e0a453a084fa1ffc2a
EreckGordon <ereckgordon@gmail.com>
2017-08-31 23:30:51 -0700

    remove blog from nav views
    this is so you can have an indeterminate number of children and still
    have the activated route show as blog selected

--- src/app/app-nav-views.ts
+++ src/app/app-nav-views.ts
@@ -8,10 +8,5 @@ export const views: Object[] = [
     name: 'About',
     icon: 'important_devices',
     link: ['about']
-  },
-  {
-    name: 'Blog',
-    icon: 'trending_up',
-    link: ['blog']
   }
 ];
