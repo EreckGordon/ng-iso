45440abfc8ba97fa883a349cdda389301c01e1f1
EreckGordon <ereckgordon@gmail.com>
2017-09-01 07:45:54 -0700

    move the exact:true call to the only link that needs it: '/'

--- src/app/app-nav-views.ts
+++ src/app/app-nav-views.ts
@@ -1,12 +1,12 @@
 export const views: Object[] = [
   {
-    name: 'Home',
-    icon: 'home',
-    link: ['']
-  },
-  {
     name: 'About',
     icon: 'important_devices',
     link: ['about']
+  },
+  {
+    name: 'Blog',
+    icon: 'trending_up',
+    link: ['blog']
   }
 ];
